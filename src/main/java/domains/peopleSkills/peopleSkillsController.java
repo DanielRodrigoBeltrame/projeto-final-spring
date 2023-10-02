4bb7278b65322d80ddb54327b984a6c06b06caf5
Daniel Rodrigo Beltrame <danielrbeltrame@gmail.com>
2023-10-01 23:27:06 -0300

    Controller peopleSkills

---
+++ src/main/java/domains/peopleSkills/peopleSkillsController.java
@@ -0,0 +1,11 @@
+package domains.peopleSkills;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/people-skill")
+@RequiredArgsConstructor
+public class peopleSkillsController {
+}
